7e2ddc96805b43f323826e0c00e8871b20cd8652
ilYa <EnableWebSocket@yandex.ru>
2025-03-23 10:53:57 +0500

    add user api tests

--- src/test/java/com/pii/tracker/track_calories/user/controller/UserControllerTest.java
+++ src/test/java/com/pii/tracker/track_calories/user/controller/UserControllerTest.java
@@ -1,9 +1,11 @@
 package com.pii.tracker.track_calories.user.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.pii.tracker.track_calories.user.exception.UserNotFoundException;
 import com.pii.tracker.track_calories.user.model.User;
 import com.pii.tracker.track_calories.user.model.WeightGoal;
 import com.pii.tracker.track_calories.user.service.UserService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,11 +20,12 @@ import java.util.List;
 import java.util.Optional;
 
 import static com.pii.tracker.track_calories.util.TestDataFactory.createTestUser;
-import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 @ExtendWith(SpringExtension.class)
@@ -31,54 +34,35 @@ public class UserControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
     @MockitoBean
     private UserService userService;
+
     @Autowired
     private ObjectMapper objectMapper;
 
+    private User user;
+
+    @BeforeEach
+    void setUp() {
+        user = createTestUser();
+    }
+
     @Test
     void createUser_ShouldReturnCreatedUser() throws Exception {
-        var user = createTestUser();
         when(userService.createUser(any(User.class))).thenReturn(user);
 
         mockMvc.perform(MockMvcRequestBuilders.post("/api/users")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .accept(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(user)))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$.name", is("vasYa")))
-                .andExpect(jsonPath("$.email", is("vasYa@ya.ru")));
-    }
-
-    @Test
-    void getAllUsers_ShouldReturnUserList() throws Exception {
-        var users = List.of(createTestUser());
-        when(userService.getAllUsers()).thenReturn(users);
-
-        mockMvc.perform(MockMvcRequestBuilders.get("/api/users")
-                        .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.size()", is(1)))
-                .andExpect(jsonPath("$[0].name", is("vasYa")));
-    }
-
-    @Test
-    void getUserById_ShouldReturnUser_WhenUserExists() throws Exception {
-        var user = createTestUser();
-        when(userService.getUserById(1L)).thenReturn(Optional.of(user));
-
-        mockMvc.perform(MockMvcRequestBuilders.get("/api/users/1")
-                        .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.name", is("vasYa")))
-                .andExpect(jsonPath("$.email", is("vasYa@ya.ru")));
+                .andExpect(jsonPath("$.name", is(user.getName())))
+                .andExpect(jsonPath("$.email", is(user.getEmail())));
     }
 
     @Test
     void createUser_ShouldReturnBadRequest_WhenEmailIsMissing() throws Exception {
         var invalidUser = User.builder()
-                .id(2L)
                 .name("noemail")
                 .email(null)
                 .weight(80)
@@ -89,10 +73,30 @@ public class UserControllerTest {
 
         mockMvc.perform(MockMvcRequestBuilders.post("/api/users")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(invalidUser))
+                        .content(objectMapper.writeValueAsString(invalidUser)))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void getAllUsers_ShouldReturnUserList() throws Exception {
+        when(userService.getAllUsers()).thenReturn(List.of(user));
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/users")
                         .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.email", containsString("Email обязателен")));
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.size()", is(1)))
+                .andExpect(jsonPath("$[0].name", is(user.getName())));
+    }
+
+    @Test
+    void getUserById_ShouldReturnUser_WhenUserExists() throws Exception {
+        when(userService.getUserById(1L)).thenReturn(Optional.of(user));
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/users/1")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(user.getName())))
+                .andExpect(jsonPath("$.email", is(user.getEmail())));
     }
 
     @Test
@@ -105,40 +109,43 @@ public class UserControllerTest {
     }
 
     @Test
-    void createUser_ShouldReturnBadRequest_WhenWeightGoalIsMissing() throws Exception {
-        var invalidUser = User.builder()
-                .name("NoGoalUser")
-                .email("user@ya.ru")
-                .weight(70)
-                .height(175)
-                .age(30)
-                .weightGoal(null)
-                .build();
+    void updateUser_ShouldReturnUpdatedUser_WhenUserExists() throws Exception {
+        when(userService.updateUser(eq(1L), any(User.class))).thenReturn(user);
 
-        mockMvc.perform(MockMvcRequestBuilders.post("/api/users")
+        mockMvc.perform(MockMvcRequestBuilders.put("/api/users/1")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(invalidUser))
-                        .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.weightGoal", containsString("Цель по изменению веса обязательна")));
+                        .content(objectMapper.writeValueAsString(user)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(user.getName())))
+                .andExpect(jsonPath("$.email", is(user.getEmail())));
     }
 
     @Test
-    void createUser_ShouldReturnBadRequest_WhenHeightIsTooHigh() throws Exception {
-        var invalidUser = User.builder()
-                .name("TallUser")
-                .email("tall@ya.ru")
-                .weight(80)
-                .height(512)
-                .age(35)
-                .weightGoal(WeightGoal.MAINTAIN)
-                .build();
+    void updateUser_ShouldReturnNotFound_WhenUserDoesNotExist() throws Exception {
+        when(userService.updateUser(eq(1L), any(User.class)))
+                .thenThrow(new UserNotFoundException("Пользователь не найден id=1"));
 
-        mockMvc.perform(MockMvcRequestBuilders.post("/api/users")
+        mockMvc.perform(MockMvcRequestBuilders.put("/api/users/1")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(invalidUser))
+                        .content(objectMapper.writeValueAsString(user)))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void deleteUser_ShouldReturnNoContent_WhenUserExists() throws Exception {
+        doNothing().when(userService).deleteUser(1L);
+
+        mockMvc.perform(MockMvcRequestBuilders.delete("/api/users/1")
                         .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.height", containsString("Рост не может быть более 256 см")));
+                .andExpect(status().isNoContent());
+    }
+
+    @Test
+    void deleteUser_ShouldReturnNotFound_WhenUserDoesNotExist() throws Exception {
+        doThrow(new UserNotFoundException("Пользователь не найден id=1")).when(userService).deleteUser(1L);
+
+        mockMvc.perform(MockMvcRequestBuilders.delete("/api/users/1")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
     }
 }
